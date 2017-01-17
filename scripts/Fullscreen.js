7c45cf38e80ffb45a118bbaf4648a7cc419ca837
Chris Luby <cluby@adobe.com>
2017-01-16 23:08:56 -0700

    Added missing Fullscreen module

---
+++ scripts/Fullscreen.js
@@ -0,0 +1,55 @@
+/*
+ * SMotion - Stop Motion Animation in a Web Browser
+ * Copyright (C) 2017 Chris Luby <clubycoder@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+"use strict";
+
+module.exports = {
+  "isEnabled": function() {
+    return ((document.fullscreenElement) ||
+            (document.webkitFullscreenElement) ||
+            (document.mozFullScreenElement) ||
+            (document.msFullscreenElement));
+  },
+  "toggleFullscreen": function(elem) {
+    if (this.isEnabled()) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    } else {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      }
+    }
+  },
+  "addOnChangeListener": function(onChange) {
+    ["", "webkit", "moz", "ms"].forEach(
+      prefix => document.addEventListener(prefix + "fullscreenchange", onChange, false)
+    );
+  }
+}
